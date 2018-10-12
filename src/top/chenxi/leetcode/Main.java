fa4516e1bb07457b4a39feec1bd1c68d1914c1cd
chenxi-ysc <1312159004@qq.com>
2018-10-12 13:04:13 +0800

    problem4

--- src/top/chenxi/leetcode/Main.java
+++ src/top/chenxi/leetcode/Main.java
@@ -6,26 +6,31 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args){
         Solution solution = new Solution();
-        /*//1.两数之和
-        int[] sums = {2,7,11,15};
-        System.out.println("1. "+Arrays.toString(solution.twoSum(sums,9)));
-        //2.两数相加
-        ListNode l1 = new ListNode(7);
-        l1.next = new ListNode(0);
-        l1.next.next = new ListNode(8);
-        ListNode l2 = new ListNode(5);
-        l2.next = new ListNode(8);
-        l2.next.next = new ListNode(7);
-        ListNode rs = solution.addTwoNumbers(l1,l2);
-        ArrayList array = new ArrayList();
-        while (rs!=null){
-            array.add(rs.val);
-            rs = rs.next;
-        }
-        Integer[] a = (Integer[])array.toArray(new Integer[array.size()]);
-        System.out.println("2. "+Arrays.toString(a));*/
-        //3.无重复字符的最长字串
-        int rs3 = solution.lengthOfLongestSubstring("pwwkew");
-        System.out.println("3. "+rs3);
+//        //1.两数之和
+//        int[] sums = {2,7,11,15};
+//        System.out.println("1. "+Arrays.toString(solution.twoSum(sums,9)));
+//        //2.两数相加
+//        ListNode l1 = new ListNode(7);
+//        l1.next = new ListNode(0);
+//        l1.next.next = new ListNode(8);
+//        ListNode l2 = new ListNode(5);
+//        l2.next = new ListNode(8);
+//        l2.next.next = new ListNode(7);
+//        ListNode rs = solution.addTwoNumbers(l1,l2);
+//        ArrayList array = new ArrayList();
+//        while (rs!=null){
+//            array.add(rs.val);
+//            rs = rs.next;
+//        }
+//        Integer[] a = (Integer[])array.toArray(new Integer[array.size()]);
+//        System.out.println("2. "+Arrays.toString(a));
+//        //3.无重复字符的最长字串
+//        int rs3 = solution.lengthOfLongestSubstring("pwwkew");
+//        System.out.println("3. "+rs3);
+        //4.两个排序数组的中位数
+        int[] nums1 = {1,2};
+        int[] nums2 = {4,5,7};
+        double rs4 = solution.findMedianSortedArrays(nums1,nums2);
+        System.out.print(rs4);
     }
 }
--- src/top/chenxi/leetcode/Solution.java
+++ src/top/chenxi/leetcode/Solution.java
@@ -3,6 +3,7 @@ package top.chenxi.leetcode;
 import java.util.HashSet;
 
 public class Solution {
+    //1.
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
@@ -11,7 +12,7 @@ public class Solution {
         }
         throw new IllegalArgumentException("not found");
     }
-
+    //2.
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int x;
         int y;
@@ -46,7 +47,7 @@ public class Solution {
         }
         return rs.next;
     }
-
+    //3.
     public int lengthOfLongestSubstring(String s) {
         HashSet<Character> set = new HashSet<>();
         int i = 0, j = 0, max = 0;
@@ -62,5 +63,36 @@ public class Solution {
             }
         }
         return max;
+    }
+    //4.
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int midLeft = (m+n+1)/2, midRight = (m+n+2)/2;
+        int mid1 = findK(nums1,0,nums2,0,midLeft);
+        int mid2 = findK(nums1,0,nums2,0,midRight);
+        double mid = (mid1+mid2)/2.0;
+        return mid;
+    }
+    int findK(int[] nums1, int i, int[] nums2, int j, int k){
+        int mid1=0, mid2=0;
+        if(i>=nums1.length) return nums2[j+k-1];
+        if(j>=nums2.length) return nums1[i+k-1];
+        if(k==1) return Math.min(nums1[i], nums2[j]);
+        if(i+k/2-1<nums1.length){
+            mid1 = nums1[i+k/2-1];
+        }
+        else
+            return findK(nums1,i,nums2,j+k/2,k-k/2);
+        if(j+k/2-1<nums2.length){
+            mid2 = nums2[j+k/2-1];
+        }
+        else
+            return findK(nums1,i+k/2,nums2,j,k-k/2);
+        if(mid1<mid2){
+            return findK(nums1,i+k/2,nums2,j,k-k/2);
+        }
+        else{
+            return findK(nums1,i,nums2,j+k/2,k-k/2);
+        }
     }
 }
